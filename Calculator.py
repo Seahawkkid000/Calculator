19418fe2e991a591f206cdf437ee5074b3b6c82b
Seahawkkid000 <107086227+Seahawkkid000@users.noreply.github.com>
2023-09-04 18:49:34 -0500

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,35 @@
+#PROGRAM TO DESIGN SIMPLE CALCULATOR#
+While(b="true"):
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+print("5. Exit")
+choice=input("Enter your choice")
+print("Enter two numbers")
+var1=int(input("enter first number")
+var2=int(input("enter second number")
+result=0
+b="true"
+if (choice=1):
+    result=var1+var2
+    print("result=",result)
+
+elif (choice=2):
+    result=var1-var2
+    print("result=",result)
+
+elif (choice=3):
+    result=var1*var3
+    print("result=",result)
+
+elif (choice=4):
+    result=var1/var2
+    print("result=",result)
+
+elif (choice=5):
+    break
+    b="false"
+else:
+    print("Wrong output")
+
